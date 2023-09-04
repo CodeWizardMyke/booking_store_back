d499dcc4ceae940894533586f301c97c3b819d43
CodeWizardMyke <maikewilliam.dev@gmail.com>
2023-09-03 21:52:40 -0300

    [fix] problema de conexão com banco de dados resolvido

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,39 +1,10 @@
 require('dotenv').config()
 
-const config = {
-  development:{
-    'username': 'postgres',
-    'password': 'u1bOjhogjpX4qW1g',
-    'database': 'postgres',
-    'host': 'db.cfsqmfjhyajjjkfnongp.supabase.co',
-    'port': '5432',
-    'dialect': 'postgres',
-  },
-  test:{
-    'username':process.env.DBUSER,
-    'password':process.env.DBPASS,
-    'database':process.env.DBNAME,
-    'host': process.env.DBHOST,
-    'port': process.env.DBPORT,
-    'dialect':'postgres',
-  },
-  production:{
-    'username':process.env.DBUSER,
-    'password':process.env.DBPASS,
-    'database':process.env.DBNAME,
-    'host': process.env.DBHOST,
-    'port': process.env.DBPORT,
-    'dialect':'postgres',
-  }
-};
-
 module.exports = {
-  production:{
-    'username':process.env.DBUSER,
-    'password':process.env.DBPASS,
-    'database':process.env.DBNAME,
-    'host': process.env.DBHOST,
-    'port': process.env.DBPORT,
-    'dialect':'postgres',
-  }
+  'username':process.env.DBUSER,
+  'password':process.env.DBPASS,
+  'database':process.env.DBNAME,
+  'host': process.env.DBHOST,
+  'port': process.env.DBPORT,
+  'dialect':'postgres',
 }
